bb4a9b037f96cab647e80d30e5ec1fd0505e65f8
V <V@LAPTOP-66GCD269>
2019-04-02 16:26:15 +0800

    update

---
+++ ArrayManipulation/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayManipulation
+{
+    class Program
+    {
+        public object Solve()
+        {
+            int n = ReadInt();
+            int m = ReadInt();
+            var data = new Tuple<int, int>[m * 2];
+            for (int i = 0; i < m; i++)
+            {
+                int a = ReadInt();
+                int b = ReadInt();
+                int k = ReadInt();
+                data[2 * i] = Tuple.Create(a, k);
+                data[2 * i + 1] = Tuple.Create(b + 1, -k);
+            }
+
+            Array.Sort(data);
+
+            long c = 0, max = 0;
+            foreach (var t in data)
+            {
+                c += t.Item2;
+                max = Math.Max(max, c);
+            }
+
+            return max;
+        }
+
+        #region Main
+
+        protected static TextReader reader;
+        protected static TextWriter writer;
+
+        static void Main()
+        {
+#if DEBUG
+            reader = new StreamReader("..\\..\\input.txt");
+            writer = Console.Out;
+            //writer = new StreamWriter("..\\..\\output.txt");
+#else
+        reader = Console.In;
+        writer = new StreamWriter(Console.OpenStandardOutput());
+#endif
+            try
+            {
+                object result = new Program().Solve();
+                if (result != null)
+                    writer.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                Console.WriteLine(ex);
+#else
+            throw;
+#endif
+            }
+            reader.Close();
+            writer.Close();
+        }
+
+        #endregion
+
+        #region Read/Write
+
+        private static Queue<string> currentLineTokens = new Queue<string>();
+
+        private static string[] ReadAndSplitLine()
+        {
+            //string[] aa;
+            //aa= reader.ReadLine().Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            return reader.ReadLine().Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public static string ReadToken()
+        {
+           
+            while (currentLineTokens.Count == 0)
+            {               
+                currentLineTokens = new Queue<string>(ReadAndSplitLine());
+            }
+            return currentLineTokens.Dequeue();
+        }
+
+        public static int ReadInt()
+        {
+            return int.Parse(ReadToken());
+        }
+
+        public static long ReadLong()
+        {
+            return long.Parse(ReadToken());
+        }
+
+        public static double ReadDouble()
+        {
+            return double.Parse(ReadToken(), CultureInfo.InvariantCulture);
+        }
+
+        public static int[] ReadIntArray()
+        {
+            return ReadAndSplitLine().Select(int.Parse).ToArray();
+        }
+
+        public static long[] ReadLongArray()
+        {
+            return ReadAndSplitLine().Select(long.Parse).ToArray();
+        }
+
+        public static double[] ReadDoubleArray()
+        {
+            return ReadAndSplitLine().Select(s => double.Parse(s, CultureInfo.InvariantCulture)).ToArray();
+        }
+
+        public static int[][] ReadIntMatrix(int numberOfRows)
+        {
+            int[][] matrix = new int[numberOfRows][];
+            for (int i = 0; i < numberOfRows; i++)
+                matrix[i] = ReadIntArray();
+            return matrix;
+        }
+
+        public static int[][] ReadAndTransposeIntMatrix(int numberOfRows)
+        {
+            int[][] matrix = ReadIntMatrix(numberOfRows);
+            int[][] ret = new int[matrix[0].Length][];
+            for (int i = 0; i < ret.Length; i++)
+            {
+                ret[i] = new int[numberOfRows];
+                for (int j = 0; j < numberOfRows; j++)
+                    ret[i][j] = matrix[j][i];
+            }
+            return ret;
+        }
+
+        public static string[] ReadLines(int quantity)
+        {
+            string[] lines = new string[quantity];
+            for (int i = 0; i < quantity; i++)
+                lines[i] = reader.ReadLine().Trim();
+            return lines;
+        }
+
+        public static void WriteArray<T>(IEnumerable<T> array)
+        {
+            writer.WriteLine(string.Join(" ", array));
+        }
+
+        #endregion
+    }
+}
--- Hackerrank1/Program.cs
+++ Hackerrank1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +16,8 @@ namespace Hackerrank1
             //int n = 5;
             //int d = 4;
             //  = reverseArray1(a,n,d);
-            int[] aa = matchingStrings(strings,queries);
+            //int[] aa = matchingStrings(strings,queries);
+            long aa = arrayManipulation(n,queries);
         }
         static int[] reverseArray(int[] a)
         {
@@ -27,26 +30,26 @@ namespace Hackerrank1
             }
             return b;
         }
-        static int[] reverseArray1(int[] a,int n,int d)
+        static int[] reverseArray1(int[] a, int n, int d)
         {
             int[] b = new int[a.Length];
             for (int i = 0; i < a.Length; i++)
             {
-                if (i-d<0)
+                if (i - d < 0)
                 {
-                    int Phasereduction = n+(i - d);
-                    b[Phasereduction]=a[i];
+                    int Phasereduction = n + (i - d);
+                    b[Phasereduction] = a[i];
                 }
-                else if(i-d>=0)
+                else if (i - d >= 0)
                 {
-                   b[i - d] = a[i] ;
+                    b[i - d] = a[i];
                 }
 
             }
             return b;
         }
-        static string[] strings = { "ab", "ab", "abc"};
-        static string[] queries = {"ab","abc","bc" };
+        //static string[] strings = { "ab", "ab", "abc"};
+        // static string[] queries = {"ab","abc","bc" };
         static int[] matchingStrings(string[] strings, string[] queries)
         {
             int[] Coutarr = new int[queries.Length];
@@ -55,7 +58,7 @@ namespace Hackerrank1
                 int aaa = 0;
                 for (int j = 0; j < strings.Length; j++)
                 {
-                    if (queries[i]==strings[j])
+                    if (queries[i] == strings[j])
                     {
                         aaa++;
                         Coutarr[i] = aaa;
@@ -63,6 +66,33 @@ namespace Hackerrank1
                 }
             }
             return Coutarr;
+        }
+       static   int n = 10;
+      static  int[][] queries = new int[3][] { new int[] { 1, 5 ,3 }, new int[] { 4, 10 ,7 }, new int[] { 6, 9 ,1 } };
+        //static int[][] queries = new int[3][] { new int[] { 1, 2, 100 }, new int[] { 2, 5, 100 }, new int[] { 3, 4, 100 } };
+
+        static long arrayManipulation(int n, int[][] queries)
+        {
+            long[] aaa = new long[n];         
+            for (long i = 0; i < queries.Length; i++)
+            {
+                    long friIndex = queries[i][0];
+                    long secIndex = queries[i][1];
+                    long addcount = queries[i][2];
+                for (long j = 0; j <secIndex-friIndex+1 ; j++)
+                {
+                    aaa[friIndex-1+j] += addcount;
+                }             
+            }
+            long max = aaa[0];
+            for (long i = 0; i < aaa.Length; i++)
+            {
+                if (aaa[i]>max)
+                {
+                    max = aaa[i];
+                }
+            }
+            return max;
         }
     }
 }
